dba49b9b951d331b90095576364761b134cbbe5d
SwiftShimizu <shimizuswiftforlearning@gmail.com>
2024-11-07 10:17:23 +0900

    イメージ対応

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,16 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  final col = const Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: const [
-        Text("レモン"),
-        Text("オレンジ"),
-        Text("バナナ"),
-      ]);
+  final col = Column(mainAxisSize: MainAxisSize.min, children: [
+    Image.asset('assets/images/drink_banana_juice.png'),
+  ]);
+
+  final row = Row(mainAxisSize: MainAxisSize.min, children: [col, col, col]);
 
   final a = MaterialApp(
     home: Scaffold(
       body: Center(
-        child: col,
+        child: row,
       ),
     ),
   );
