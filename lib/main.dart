ec4891e79d2203fe2aaaffe8e01333a773a92498
SwiftShimizu <shimizuswiftforlearning@gmail.com>
2024-11-09 16:56:57 +0900

    ボタン実装

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,27 @@ void main() {
     number: 3,
   );
 
+  xxxx() {
+    debugPrint('通信を開始します');
+    debugPrint('通信中です');
+    debugPrint('通信が完了しました');
+  }
+
+  final button = ElevatedButton(
+    onPressed: xxxx,
+    child: Text(
+      'ボタン',
+      style: TextStyle(color: Colors.white),
+    ),
+    style: ElevatedButton.styleFrom(
+      backgroundColor: Colors.green,
+    ),
+  );
+
   final a = MaterialApp(
     home: Scaffold(
       body: Center(
-        child: bananaCounter,
+        child: button,
       ),
     ),
   );
