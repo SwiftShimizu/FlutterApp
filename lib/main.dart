57ff1f9c73135d712873a99abd2a945c851e8a9a
SwiftShimizu <shimizuswiftforlearning@gmail.com>
2024-11-10 17:12:36 +0900

    BottomNavigationBar対応

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,66 @@
 import 'package:flutter/material.dart';
-import 'package:flutterapp/side_menu.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutterapp/page_a.dart';
+import 'package:flutterapp/page_b.dart';
+import 'package:flutterapp/page_c.dart';
 
 void main() {
-  final appBar = AppBar(
-    title: const Text('Flutter Example'),
-    backgroundColor: Colors.blue,
+  const app = MaterialApp(
+    home: const Root(),
   );
+  runApp(ProviderScope(child: app));
+}
 
-  // ドロワー
-  const drawer = Drawer(
-    child: SideMenu(),
-  );
+// プロバイダー
+final indexProvider = StateProvider(
+  (ref) {
+    return 0;
+  },
+);
 
-  // エンドドロワー
-  const endDrawer = Drawer(
-    child: SideMenu(),
-  );
+class Root extends ConsumerWidget {
+  const Root({super.key});
 
-  final scaffold = Scaffold(
-    appBar: appBar,
-    drawer: drawer,
-    endDrawer: endDrawer,
-  );
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final index = ref.watch(indexProvider);
 
-  final app = MaterialApp(
-    home: scaffold,
-  );
+    // ボトムナビケーションバーアイテム
+    final items = [
+      BottomNavigationBarItem(
+        icon: Icon(Icons.home),
+        label: 'Home',
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.settings),
+        label: 'Settings',
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.person),
+        label: 'Profile',
+      ),
+    ];
 
-  runApp(app);
+    final bar = BottomNavigationBar(
+      items: items,
+      backgroundColor: Colors.deepOrangeAccent,
+      selectedItemColor: Colors.white,
+      unselectedItemColor: Colors.black,
+      currentIndex: index,
+      onTap: (int index) {
+        ref.read(indexProvider.notifier).state = index;
+      },
+    );
+
+    final pages = [
+      PageA(),
+      PageB(),
+      PageC(),
+    ];
+
+    return Scaffold(
+      body: pages[index],
+      bottomNavigationBar: bar,
+    );
+  }
 }
