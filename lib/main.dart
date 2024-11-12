14d6e8ed444999323ff61b4c1495120cd1e21527
SwiftShimizu <shimizuswiftforlearning@gmail.com>
2024-11-12 22:01:58 +0900

    プログレスバー対応

--- lib/main.dart
+++ lib/main.dart
@@ -1,109 +1,71 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:riverpod/riverpod.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
+import 'package:percent_indicator/linear_percent_indicator.dart';
 
 void main() {
-  runApp(const ProviderScope(
-    child: MyApp(),
-  ));
+  const app = MaterialApp(
+    home: const Home(),
+  );
+  const scope = ProviderScope(child: app);
+  runApp(scope);
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Home(),
-    );
-  }
-}
-
-// 選ばれたラジオボタンの ID を保持する Provider
-final radioIdProvider = StateProvider<String?>((ref) => null);
-
-// 選ばれたチェックボックスのIDたちを保持する Provider
-final checkedIdsProvider = StateProvider<Set<String>>((ref) => {});
+// どれくらい進んだかを表示する　パーセント
+final percentProvider = StateProvider((ref) {
+  return 0.00;
+});
 
 class Home extends ConsumerWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    // ラジオボタンIDに合わせて画面を変化させる
-    final radioId = ref.watch(radioIdProvider);
-    // チェックボックスのIDたちに合わせて画面を変化させる
-    final checkedIds = ref.watch(checkedIdsProvider);
+    final percent = ref.watch(percentProvider);
 
-    // ラジオボタンを押された時の処理
-    void onRadioChanged(String? id) {
-      ref.read(radioIdProvider.notifier).state = id;
-    }
+    final circular = CircularPercentIndicator(
+      percent: percent,
+      backgroundColor: Colors.yellow,
+      progressColor: Colors.green,
+      radius: 60,
+      lineWidth: 20,
+      center: Text('${percent * 100} %'),
+      animation: true,
+      animateFromLastPercent: true,
+      animationDuration: 200,
+    );
 
-    // チェックボックスを押された時の処理
-    void onChangedCheckbox(String id) {
-      final newSet = Set.of(checkedIds);
-      if (newSet.contains(id)) {
-        newSet.remove(id);
-      } else {
-        newSet.add(id);
-      }
-      ref.read(checkedIdsProvider.notifier).state = newSet;
-    }
+    final linear = LinearPercentIndicator(
+      percent: percent,
+      backgroundColor: Colors.yellow,
+      progressColor: Colors.green,
+      lineHeight: 20,
+      alignment: MainAxisAlignment.center,
+      width: 300,
+      animation: true,
+      animateFromLastPercent: true,
+      animationDuration: 200,
+      center: Text('${percent * 100} %'),
+    );
+
+    final button = ElevatedButton(
+      onPressed: () {
+        ref.read(percentProvider.notifier).state += 0.1;
+      },
+      child: const Text('進める'),
+    );
 
     final col = Column(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        // ラジオボタングループ
-        RadioListTile(
-          groupValue: radioId,
-          onChanged: onRadioChanged,
-          value: 'A',
-          title: const Text('ラジオA'),
-        ),
-        RadioListTile(
-          groupValue: radioId,
-          onChanged: onRadioChanged,
-          value: 'B',
-          title: const Text('ラジオB'),
-        ),
-        RadioListTile(
-          groupValue: radioId,
-          onChanged: onRadioChanged,
-          value: 'C',
-          title: const Text('ラジオC'),
-        ),
-
-        // チェックボックスグループ
-        CheckboxListTile(
-          onChanged: (value) => onChangedCheckbox('1'),
-          title: const Text('チェックボックス1'),
-          value: checkedIds.contains('1'),
-        ),
-        CheckboxListTile(
-          onChanged: (value) => onChangedCheckbox('2'),
-          title: const Text('チェックボックス2'),
-          value: checkedIds.contains('2'),
-        ),
-        CheckboxListTile(
-          onChanged: (value) => onChangedCheckbox('3'),
-          title: const Text('チェックボックス3'),
-          value: checkedIds.contains('3'),
-        ),
-
-        ElevatedButton(
-          onPressed: () {
-            debugPrint('ラジオボタンの値: $radioId');
-            debugPrint('チェックボックスの値: $checkedIds');
-          },
-          child: const Text('Submit'),
-        ),
+        circular,
+        const SizedBox(height: 50),
+        linear,
+        const SizedBox(height: 50),
+        button,
       ],
     );
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-      ),
-      body: Center(child: col),
-    );
+
+    return Scaffold(body: col);
   }
 }
